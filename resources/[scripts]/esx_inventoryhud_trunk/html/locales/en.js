ddca7a7b708a0bed18d7ea1b50fdaf429ca880ff
SoHeil-R <soheildkta@gmail.com>
2020-06-18 14:30:07 -0700

    ViceCity
    ViceCity

---
+++ resources/[scripts]/esx_inventoryhud_trunk/html/locales/en.js
@@ -0,0 +1,7 @@
+var invLocale = new Object();
+invLocale.dropItem = "Drop";
+invLocale.useItem = "Use";
+invLocale.giveItem = "Give";
+invLocale.craftItem = "Craft";
+
+invLocale.secondInventoryNotAvailable = "Second inventory is not available";
